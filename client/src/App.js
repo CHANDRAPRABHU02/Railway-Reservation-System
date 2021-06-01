aee664a75a6358f65486194a8acccb82760e3f12
CHANDRAPRABHU02 <chandraprabhu02@gmail.com>
2021-06-01 16:39:34 +0530

    Added backend files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Button,
+  Nav,
+  Form,
+  FormControl,
+  Navbar,
+  Container,
+} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <h1>Home Page</h1>;
+    // return <Router> //   <Route path = "/" component = {} // </Router>
+  }
 }
 
 export default App;
